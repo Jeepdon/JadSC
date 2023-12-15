df159042399bd2814e4919a2434da2a42ff43387
Ja90n <jadonvanbeilen@gmail.com>
2023-12-15 17:43:34 +0100

    first tests

--- src/main/java/com/jadonvb/Main.java
+++ src/main/java/com/jadonvb/Main.java
@@ -5,7 +5,10 @@ import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.server.ServerInfo;
 import org.slf4j.Logger;
+
+import java.net.InetSocketAddress;
 
 @Plugin(
         id = "jadsc",
@@ -15,11 +18,18 @@ import org.slf4j.Logger;
 )
 public class Main {
 
-    @Inject
     private Logger logger;
+    private ProxyServer proxyServer;
+
+    @Inject
+    public Main(ProxyServer proxyServer, Logger logger) {
+        this.logger = logger;
+        this.proxyServer = proxyServer;
+    }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
-        
+        ServerInfo serverInfo = new ServerInfo("sumo", InetSocketAddress.createUnresolved("0",30065));
+        proxyServer.registerServer(serverInfo);
     }
 }
