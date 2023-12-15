adc6b502940e52aab49223c4f02c65eb92b09b36
Ja90n <jadonvanbeilen@gmail.com>
2023-12-15 17:12:21 +0100

    Initial commit

---
+++ src/main/java/com/jadonvb/Main.java
@@ -0,0 +1,25 @@
+package com.jadonvb;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import org.slf4j.Logger;
+
+@Plugin(
+        id = "jadsc",
+        name = "JadSC",
+        version = "0.0.1",
+        authors = {"Ja90n"}
+)
+public class Main {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        
+    }
+}
